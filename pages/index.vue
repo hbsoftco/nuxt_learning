b8d73d04b5a576b2ca7fd7e7bced486398ae4a59
hbsoftco <hossein.bajan@gmail.com>
2019-11-23 22:02:20 +0330

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div>
+      <div>
+        <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+        <b-modal id="modal-1" title="BootstrapVue">
+          <p class="my-4">Hello from modal!</p>
+        </b-modal>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.container {
+  .hbsoft {
+    background: red;
+  }
+}
+</style>
