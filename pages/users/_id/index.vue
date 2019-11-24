11c6dd546882ddb52305ec83bc2322e7280c7898
hbsoftco <hossein.bajan@gmail.com>
2019-11-24 05:30:12 +0330

    Update

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mt-5">
+    <div>This is user's id {{$route.params.id}}</div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    // validate(context) {
+    return /^\d+$/.test(params.id);
+    // return /^\d+$/.test(context.params.id);
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mt-5">
+    <b-input-group size="sm" class="mb-3" prepend="Type user id">
+      <b-form-input v-model="id"></b-form-input>
+      <b-input-group-append>
+        <b-button size="sm" text="Button" variant="success" @click="loadUser">Go</b-button>
+      </b-input-group-append>
+    </b-input-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: ""
+    };
+  },
+  methods: {
+    loadUser() {
+      this.$router.push(`/users/${this.id}`);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
